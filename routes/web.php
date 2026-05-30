17e5b1564f2da62dbb66be9dc9f5fcad1c944eb9
ahlysong-y <ahlysong8@gmail.com>
2026-05-31 05:02:45 +0700

    Fix web route to redirect to admin automatically

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+// 🛠️ កែប្រែត្រង់នេះ៖ ឱ្យវាបង្វែរទិសដៅ (Redirect) ទៅកាន់ផ្លូវ /admin ដោយស្វ័យប្រវត្តិ
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
