9bfa0bd22127eeff0e135534ac8c0b826bbfef2d
ahlysong-y <ahlysong8@gmail.com>
2026-06-01 17:40:25 +0700

    Fix api index bootstrap

--- api/index.php
+++ api/index.php
@@ -3,9 +3,18 @@
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/** @var \Illuminate\Foundation\Application $app */
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
 $request = Request::capture();
 
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+if (
+    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https'
+) {
     $request->server->set('HTTPS', 'on');
 }
 
