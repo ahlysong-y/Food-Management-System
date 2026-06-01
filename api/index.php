a8fa88d1c862cb0783099d2a12f86f6fdc42e206
ahlysong-y <ahlysong8@gmail.com>
2026-06-02 05:17:54 +0700

    Fix Vercel storage path

--- api/index.php
+++ api/index.php
@@ -6,8 +6,29 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$storagePath = '/tmp/storage';
+
+$directories = [
+    $storagePath . '/app/public',
+    $storagePath . '/framework/cache/data',
+    $storagePath . '/framework/sessions',
+    $storagePath . '/framework/views',
+    $storagePath . '/logs',
+];
+
+foreach ($directories as $directory) {
+    if (!is_dir($directory)) {
+        mkdir($directory, 0755, true);
+    }
+}
+
+$_ENV['APP_STORAGE'] = $storagePath;
+putenv('APP_STORAGE=' . $storagePath);
+
 /** @var \Illuminate\Foundation\Application $app */
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath($storagePath);
 
 $request = Request::capture();
 
