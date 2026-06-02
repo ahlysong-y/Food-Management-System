2b74d8045aaa8360d076a56ee38cf91d330d0b49
ahlysong-y <ahlysong8@gmail.com>
2026-06-02 14:33:01 +0700

    Fix Vercel entry point

--- api/index.php
+++ api/index.php
@@ -1,41 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
-
 define('LARAVEL_START', microtime(true));
 
-require __DIR__ . '/../public/index.php';
-
-$storagePath = '/tmp/storage';
-
-$directories = [
-    $storagePath . '/app/public',
-    $storagePath . '/framework/cache/data',
-    $storagePath . '/framework/sessions',
-    $storagePath . '/framework/views',
-    $storagePath . '/logs',
-];
-
-foreach ($directories as $directory) {
-    if (! is_dir($directory)) {
-        mkdir($directory, 0755, true);
-    }
-}
-
-$_ENV['APP_STORAGE'] = $storagePath;
-putenv('APP_STORAGE=' . $storagePath);
+require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$app->useStoragePath($storagePath);
-
-$request = Request::capture();
-
-if (
-    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
-    $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https'
-) {
-    $request->server->set('HTTPS', 'on');
-}
-
-$app->handleRequest($request);
+$app->handleRequest(
+    Illuminate\Http\Request::capture()
+);
